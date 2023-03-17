3e187324744a58a2da2cec4d9b117129a9d69e62
Danilo Ebúrneo <97124686+daneburneo@users.noreply.github.com>
2023-03-17 09:13:48 +0900

    Add files via upload
    Orientação a Objetos

---
+++ Livro.java
@@ -0,0 +1,13 @@
+
+package OrientaçaoObjeto;
+
+
+public class Livro { // aqui nao precisa do main, é a crianção de uma classe.
+    
+    String nome;
+    String autor;
+    int qtdPaginas;
+    int anoLancamento;
+    
+    
+}
